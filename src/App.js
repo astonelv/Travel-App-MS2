4c17e5d24e6abf205378da8b4d901106d46151bb
kpan13 <kajaljpan@gmail.com>
2024-07-20 13:41:46 -0700

    hotel.js, hotel.css and assets folder created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 
 function App() {
   return (
     <div className="App">
+      <hotels/>
+      <h1>Travel</h1>
+      <image src = {logo} className = "App-logo" alt = "logo"/>
+      
       
     </div>
   );

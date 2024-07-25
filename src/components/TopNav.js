bd543dfc9b662ae9f5ee7e15c7ec60f3bd3d901a
Your GitHub Username <davilama@unlv.nevada.edu>
2024-07-24 18:22:26 -0700

    updated map

---
+++ src/components/TopNav.js
@@ -0,0 +1,17 @@
+// client/src/components/TopNav.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TopNav = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default TopNav;
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -7,18 +7,23 @@ const WorldMap = () => {
     const [places, setPlaces] = useState([
         { id: 1, name: "Paris", description: "The city of love", lat: 48.8566, lng: 2.3522 },
         { id: 2, name: "New York", description: "The Big Apple", lat: 40.7128, lng: -74.0060 },
+        { id: 2, name: "Tokyo", description: "Heart of Japan", lat: 35.689487, lng: 139.691711 },
+        { id: 2, name: "Las Vegas", description: "Poker-City", lat: 36.1699, lng: -115.1398 },
+
+        
         // Add more places as needed
       ]);
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/places')
-      .then(response => {
-        setPlaces(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+      useEffect(() => {
+        axios.get('http://localhost:3000/places')
+          .then(response => {
+            setPlaces(response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, []);
+      
 
   return (
     <MapContainer center={[0, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
@@ -27,7 +32,7 @@ const WorldMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {places.map(place => (
-        <Marker key={place._id} position={[place.lat, place.lng]}>
+        <Marker key={place.id} position={[place.lat, place.lng]}>
           <Popup>
             <strong>{place.name}</strong>
             <p>{place.description}</p>
